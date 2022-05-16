c9e7c3d3c920ed83a2c65a7052facd4b60fbbbdf
dmitriy-komarov <dmitriy-komarov@3627127.no-reply.drupal.org>
2022-05-16 17:39:26 +0300

    API key changed

--- file-manager/plugin.js
+++ file-manager/plugin.js
@@ -33,7 +33,7 @@ function getCookie(name) {
 }
 
 
-apiKey = getCookie("N1ED_APIKEY") || apiKey || "TINYFLMN";
+apiKey = getCookie("N1ED_APIKEY") || apiKey || "FLMNFLMN";
 n1edHttps = !(getCookie("N1ED_HTTPS") === "false" || n1edHttps === false);
 n1edPrefix = getCookie("N1ED_PREFIX") || n1edPrefix || null;
 n1edHttpsApp = !(getCookie("N1ED_HTTPS_APP") === "false" || n1edHttpsApp === false);
