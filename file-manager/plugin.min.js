ffc2a3db40096e3b1eaea1742591da2a67d4b733
dmitriy-komarov <dmitriy-komarov@3627127.no-reply.drupal.org>
2023-08-04 21:45:46 +0400

    Version number added

--- file-manager/plugin.min.js
+++ file-manager/plugin.min.js
@@ -21,6 +21,7 @@ var n1edHttps;
 var n1edPrefixApp;
 var n1edHttpsApp;
 var urlCache;
+window.n1edPluginVersion=202308001;
 if (tinymce.majorVersion == 6) {
     var getOption = function(name, type) {
         var options = tinymce.get()[0].options;
